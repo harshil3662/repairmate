dda88cc597783e6cafd4d26b24696875c6fea521
harshil3662 <vaghaniharshil11291@gmail.com>
2024-08-17 17:53:44 -0400

    modularising backend side

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,9 @@
-var express = require('express')
-const mongoose = require('mongoose');
-const cors = require('cors')
-var bodyParser = require('body-parser')
-const mechanicModel = require('./middleware/database')
+import express from 'express'
+import 'dotenv/config'
 
-require('dotenv').config({ path: "../.env" })
-const mongoString2 = process.env.MONGO_STRING_2
-mongoose.connect(mongoString2)
-const db = mongoose.connection
-db.on('error', (error) => {
-   console.log(error)
-})
-db.once('connected', () => {
-   console.log('Database Connected to repairmate.')
-})
+const app = express()
+const port = process.env.PORT || 8000
 
-var app = express()
-
-app.use( bodyParser.json())       
-app.use(bodyParser.urlencoded({     
-  extended: true
-}))
-app.use(cors({
-   origin: ['http://localhost:3000','http://localhost:3001'],
-   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-   credentials: true,
-}))
-
-app.get("/garages", async function (req, res) {
-   var data = await mechanicModel.find({})
-   const garages = {list:data}
-   res.json(garages)
-})
-
-app.listen(8080, function () {
+app.listen(port,()=>{
+    console.log('Server is running on port number ', port);
 })
